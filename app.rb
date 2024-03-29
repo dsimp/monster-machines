eda4c46b7fcb89a100b1a72d104b27a8414035f1
Djvon Simpson <55302995+dsimp@users.noreply.github.com>
2024-03-29 17:00:52 +0000

    first files start

---
+++ app.rb
@@ -0,0 +1,4 @@
+require_relative "monster"
+
+monster = Monster.new
+monster.start
---
+++ monster.rb
@@ -0,0 +1,12 @@
+class Monster
+  attr_accessor :machine_name, :trainer_name
+
+  def initialize
+    @machine_name = machine_name
+    @trainer_name = machine_name
+  end
+
+  def start
+    puts "\n"
+    puts "Hello there, trainer!"
+  end
